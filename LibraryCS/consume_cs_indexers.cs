45d97460c1796b338da38f48dd0b64c0e85ac5c2
Xiang Fu <XFu@intl.isdgroup.com>
2017-05-29 15:59:36 +0200

    LibraryCS

---
+++ LibraryCS/consume_cs_indexers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryCS
+{
+    public class IndexerClass
+    {
+        private int[] myArray = new int[100];
+        public int this[int index]
+        {
+            get 
+            {
+                if (index < 0 || index >= 100)
+                    return 0;
+                else
+                    return myArray[index];
+            }
+            set
+            {
+                if (!(index < 0 || index >= 100))
+                    myArray[index] = value;
+            }
+        }
+    }
+
+    public class MainClass
+    {
+        public static void Main()
+        {
+            IndexerClass b = new IndexerClass();
+            
+            b[3] = 256;
+            b[5] = 1024;
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine("Element #{0} = {1}", i, b[i]);
+            }
+        }
+    }
+}
