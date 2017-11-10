35a9028c9bdaa3b7c485e601c65de6e06d2952c1
Xiang Fu <XFu@intl.isdgroup.com>
2017-11-10 14:39:32 +0100

    parser

--- Parser/CalcParser.cpp
+++ Parser/CalcParser.cpp
@@ -6,7 +6,25 @@
 //########################################################
 
 // Parser.cpp
+//http://www.qingpingshan.com/jb/python/173159.html
+
 
 #include "stdafx.h"
 #include "CalcParser.h"
 
+
+
+using namespace std;
+using namespace XFU;
+
+
+
+bool Parser::Run()
+{
+
+   return false;
+}
+
+void Parser::NextToken()
+{
+}
--- Parser/CalcParser.h
+++ Parser/CalcParser.h
@@ -7,4 +7,41 @@
 
 //四则运算语法分析器
 
+//递归下降的语法:
+//Expr   ->  Term + Expr
+//       |   Term - Expr
+//       |   Term
+//Term   ->  Factor * Term
+//       |   Factor / Term
+//       |   Factor
+//Factor ->  (Expr)
+//       |   num
+
+//根据语法生成语法树AST
+
 #pragma once
+#include <string>
+#include <vector>
+
+
+namespace XFU
+{
+   class Parser
+   {
+   public:
+      Parser(const std::vector<std::wstring>& tokens)
+         :_tokenit(tokens.begin())
+      {}
+
+      ~Parser()
+      {}
+
+      bool Run();
+
+   private:
+      void NextToken();
+
+   private:
+      std::vector<std::wstring>::const_iterator _tokenit;
+   };
+}
