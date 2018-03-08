68a2134772a23045cce3e782a8971744afd2f4a4
rich <tech@zhongwenshu.de>
2018-03-08 10:15:37 +0100

    time

--- Tool/Time.h
+++ Tool/Time.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <time.h>
 
 namespace XFU
 {
@@ -11,5 +12,19 @@ namespace XFU
       __int64 begin = GetTickCount();
       f();
       return GetTickCount() - begin;
+   }
+
+   //Start-Zeit definition, diese Zeit darf nie geändert werden!
+   const static __time64_t utcStartTime = 1397058688; //Dortmund 9.4.2014 17:50
+
+   //aktuelle UTC-Zeit als 32bit Integer umrechnen
+   int current_utc_time_to_int32()
+   {
+      __time64_t t;
+      _time64(&t);
+
+      __time64_t diff = t - utcStartTime;
+
+      return static_cast<int>(diff);
    }
 }
