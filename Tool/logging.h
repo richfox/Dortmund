289aa8bcd17050caa84d5b323331d97a3228744d
rich <tech@zhongwenshu.de>
2019-05-10 13:36:32 +0200

    logging

--- Tool/logging.h
+++ Tool/logging.h
@@ -9,13 +9,16 @@ namespace XFU
    void log_message(const std::wstring& file,const std::wstring& message)
    {
       std::wofstream ofs(file.c_str(),std::ios::app); //append to end of file
-      __time64_t utc;
-      _time64(&utc);
-      char tbuf[128] = "";
-      char dbuf[128] = "";
-      _strtime_s(tbuf,_countof(tbuf));
-      _strdate_s(dbuf,_countof(dbuf));
-      ofs << "[" << dbuf << ", " << tbuf << ", " << utc << "] " << message << "\n";
-      ofs.close();
+      if (ofs.is_open())
+      {
+         __time64_t utc;
+         _time64(&utc);
+         char tbuf[128] = "";
+         char dbuf[128] = "";
+         _strtime_s(tbuf,_countof(tbuf));
+         _strdate_s(dbuf,_countof(dbuf));
+         ofs << "[" << dbuf << ", " << tbuf << ", " << utc << "] " << message << "\n";
+         ofs.close();
+      }
    }
 }
