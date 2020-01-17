977ca4736ffe23d41d7532b31942d3326da471a2
xfu <xfu@DEDTM004>
2020-01-17 18:44:49 +0100

    物流表达式

--- Parser/LogisTree.h
+++ Parser/LogisTree.h
@@ -9,31 +9,89 @@
 //%铁路(中通6666 + 圆通YT(8888 + 5641)) + %空运(JD0001-1-1)
 
 
+//  (  表达式的根 %铁路(中通6666 + 圆通YT(8888 + 5641)) + %空运(JD0001-1-1) ) 
+//               |                        |                         |
+//             %铁路                      +                        %空运       
+//      /        |         \                                        |
+// 中通6666      +    圆通YT(8888 + 5641)                        JD0001-1-1
+//                    /            \
+//                 圆通YT        8888 + 5641
+//                              /     |     \
+//                           8888     +     5641
+
+
+
 #pragma once
+#include <string>
 
-enum class NodeType
+
+namespace XFU
 {
-   Undef = -1,
-   Leaf,
-   Key  //%Foo()
-};
-
-class LogisTreeNode abstract
-{
-public:
-   LogisTreeNode()
-      :_type(NodeType::Undef)
-   {}
-
-   explicit LogisTreeNode(const NodeType type)
-      :_type(type)
-   {}
-
-   NodeType GetType() const
+   enum class NodeType
    {
-      return _type;
-   }
+      Undef = -1,
+      Keyword,  //%Foo
+      Text, //中通6666
+      Operator, //+
+      Expr //8888 + 5641
+   };
 
-private:
-   NodeType _type;
-};
+   class LogisTreeNode abstract
+   {
+   public:
+      LogisTreeNode()
+         :_type(NodeType::Undef)
+      {}
+
+      explicit LogisTreeNode(const NodeType type)
+         :_type(type)
+      {}
+
+      NodeType GetType() const
+      {
+         return _type;
+      }
+
+   private:
+      NodeType _type;
+   };
+
+   class LogisTreeText final : public LogisTreeNode
+   {
+   public:
+      LogisTreeText(const std::wstring& text)
+         :LogisTreeNode(NodeType::Text),
+         _text(text)
+      {}
+
+      const std::wstring& GetText() const
+      {
+         return _text;
+      }
+
+   private:
+      std::wstring _text;
+   };
+
+   class LogisTreeOperator final : public LogisTreeNode
+   {
+   public:
+      LogisTreeOperator(const std::wstring& op)
+         :LogisTreeNode(NodeType::Operator),
+         _op(op)
+      {}
+
+      const std::wstring& GetOperator() const
+      {
+         return _op;
+      }
+
+   private:
+      std::wstring _op;
+   };
+
+   class LogisTreeKey final : public LogisTreeNode
+   {
+
+   };
+}
