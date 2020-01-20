0d7bc65062350ed06dd5492879de37264e40a60e
xfu <xfu@DEDTM004>
2020-01-20 17:43:23 +0100

    消除左递归后的文法

--- Parser/LogisTree.h
+++ Parser/LogisTree.h
@@ -19,79 +19,175 @@
 //                              /     |     \
 //                           8888     +     5641
 
-
+//递归下降分析器的构造方法就是为每一个非终结符写一个递归函数，
+//函数中对该非终结符可能转换成的所有情况的第一个token进行判断，然后做出对应的处理
+//消除左递归后的文法
+//Exp -> keyword (Exp) Exp'
+//     | Text Exp'
+//Exp' -> + Exp''
+//      | null
+//Exp'' -> keyword (Exp)
+//       | Text
+//Text -> Header Factor
+//      | Factor
+//Factor -> ( sn Tail )
+//        | sn
+//Tail -> + sn Tail
+//      | null
 
 #pragma once
+
 #include <string>
+#include <vector>
+#include <memory>
 
 
 namespace XFU
 {
-   enum class NodeType
+   enum class LogisNodeType
    {
       Undef = -1,
-      Keyword,  //%Foo
-      Text, //中通6666
-      Operator, //+
-      Expr //8888 + 5641
+      Root,
+      Exp,
+      Exp1,
+      Exp2,
+      Text,
+      Factor,
+      Tail
    };
 
-   class LogisTreeNode abstract
+   class LogisTreeNode
    {
    public:
       LogisTreeNode()
-         :_type(NodeType::Undef)
+         :_type(LogisNodeType::Undef),
+         _children()
       {}
 
-      explicit LogisTreeNode(const NodeType type)
-         :_type(type)
+      explicit LogisTreeNode(const LogisNodeType type)
+         :_type(type),
+         _children()
       {}
 
-      NodeType GetType() const
+      LogisTreeNode(const LogisNodeType type,const std::shared_ptr<LogisTreeNode>& child)
+         :_type(type),
+         _children(1,child)
+      {}
+
+      LogisTreeNode(const LogisNodeType type,const std::vector<std::shared_ptr<LogisTreeNode>>& children)
+         :_type(type),
+         _children(children)
+      {}
+
+      virtual ~LogisTreeNode()
+      {}
+
+      const LogisNodeType GetType() const
       {
          return _type;
       }
 
-   private:
-      NodeType _type;
-   };
-
-   class LogisTreeText final : public LogisTreeNode
-   {
-   public:
-      LogisTreeText(const std::wstring& text)
-         :LogisTreeNode(NodeType::Text),
-         _text(text)
-      {}
-
-      const std::wstring& GetText() const
+      void AddChild(const std::shared_ptr<LogisTreeNode>& child)
       {
-         return _text;
+         _children.push_back(child);
+      }
+
+      bool HasChild() const
+      {
+         return !_children.empty();
+      }
+
+      std::shared_ptr<LogisTreeNode> GetChild(int idx) const
+      {
+         return _children[idx];
       }
 
    private:
-      std::wstring _text;
+      LogisNodeType _type;
+      std::vector<std::shared_ptr<LogisTreeNode>> _children;
    };
 
-   class LogisTreeOperator final : public LogisTreeNode
+
+   //Exp -> keyword (Exp) Exp'
+   //     | Text Exp'
+   class LogisTreeNodeExp final : public LogisTreeNode
    {
    public:
-      LogisTreeOperator(const std::wstring& op)
-         :LogisTreeNode(NodeType::Operator),
-         _op(op)
+      LogisTreeNodeExp()
+         :LogisTreeNode(LogisNodeType::Exp)
       {}
 
-      const std::wstring& GetOperator() const
-      {
-         return _op;
-      }
-
-   private:
-      std::wstring _op;
+      ~LogisTreeNodeExp()
+      {}
    };
 
-   class LogisTreeKey final : public LogisTreeNode
-   {
 
+   //Exp' -> + Exp''
+   //      | null
+   class LogisTreeNodeExp1 final : public LogisTreeNode
+   {
+   public:
+      LogisTreeNodeExp1()
+         :LogisTreeNode(LogisNodeType::Exp1)
+      {}
+
+      ~LogisTreeNodeExp1()
+      {}
+   };
+
+
+   //Exp'' -> keyword (Exp)
+   //       | Text
+   class LogisTreeNodeExp2 final : public LogisTreeNode
+   {
+   public:
+      LogisTreeNodeExp2()
+         :LogisTreeNode(LogisNodeType::Exp2)
+      {}
+
+      ~LogisTreeNodeExp2()
+      {}
+   };
+
+
+   //Text -> Header Factor
+   //      | Factor
+   class LogisTreeNodeText final : public LogisTreeNode
+   {
+   public:
+      LogisTreeNodeText()
+         :LogisTreeNode(LogisNodeType::Text)
+      {}
+
+      ~LogisTreeNodeText()
+      {}
+   };
+
+
+   //Factor -> ( sn Tail )
+   //        | sn
+   class LogisTreeNodeFactor final : public LogisTreeNode
+   {
+   public:
+      LogisTreeNodeFactor()
+         :LogisTreeNode(LogisNodeType::Factor)
+      {}
+
+      ~LogisTreeNodeFactor()
+      {}
+   };
+
+
+   //Tail -> + sn Tail
+   //      | null
+   class LogisTreeNodeTail final : public LogisTreeNode
+   {
+   public:
+      LogisTreeNodeTail()
+         :LogisTreeNode(LogisNodeType::Tail)
+      {}
+
+      ~LogisTreeNodeTail()
+      {}
    };
 }
