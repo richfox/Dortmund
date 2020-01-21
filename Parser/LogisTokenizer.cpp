e044a684ccb92c791b8141a058c2adad520af9f1
xfu <xfu@DEDTM004>
2020-01-21 17:14:27 +0100

    scanning

---
+++ Parser/LogisTokenizer.cpp
@@ -0,0 +1,31 @@
+﻿//########################################################
+//##                                                    ##
+//##        Author:    xfu                              ##
+//##        E-Mail:    tech@zhongwenshu.de              ##
+//##                                                    ##
+//########################################################
+
+//物流表达式
+//%铁路(中通6666 + 圆通YT(8888 + 5641)) + %空运(JD0001-1-1)
+
+#include "stdafx.h"
+#include "LogisTokenizer.h"
+#include <regex>
+#include <memory>
+
+
+using namespace std;
+using namespace XFU;
+
+
+vector<wstring> LogisTokenizer::Run()
+{
+   vector<wstring> tokens;
+
+   for (wstring::const_iterator it=_expr.begin(); it!=_expr.end(); it++)
+   {
+
+   }
+
+   return tokens;
+}
---
+++ Parser/LogisTokenizer.h
@@ -0,0 +1,36 @@
+﻿//########################################################
+//##                                                    ##
+//##        Author:    xfu                              ##
+//##        E-Mail:    tech@zhongwenshu.de              ##
+//##                                                    ##
+//########################################################
+
+//物流表达式
+//%铁路(中通6666 + 圆通YT(8888 + 5641)) + %空运(JD0001-1-1)
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace XFU
+{
+   class LogisTokenizer
+   {
+   public:
+      LogisTokenizer(const std::wstring& expr)
+         :_expr(expr)
+      {}
+
+      ~LogisTokenizer()
+      {}
+
+      std::vector<std::wstring> Run();
+
+   private:
+      bool IsOperator(std::wstring::const_iterator it);
+
+   private:
+      std::wstring _expr;
+   };
+}
