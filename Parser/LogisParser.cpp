5fe6822494f9d443f621110e1e5fb151ab683044
xfu <xfu@DEDTM004>
2020-01-30 18:54:17 +0100

    parsing

---
+++ Parser/LogisParser.cpp
@@ -0,0 +1,115 @@
+﻿//########################################################
+//##                                                    ##
+//##        Author:    xfu                              ##
+//##        E-Mail:    tech@zhongwenshu.de              ##
+//##                                                    ##
+//########################################################
+
+//物流表达式
+//%铁路(中通6666 + 圆通YT(8888 + 5641)) + %空运(JD0001-1-1)
+
+#include "stdafx.h"
+#include "LogisParser.h"
+#include <regex>
+#include <memory>
+#include "LogisTree.h"
+
+
+using namespace std;
+using namespace XFU;
+
+
+
+
+shared_ptr<LogisTreeNode> LogisParser::Run()
+{
+   shared_ptr<LogisTreeNode> tree(new LogisTreeNode(LogisNodeType::Root));
+   _currentnode = tree.get();
+
+   _tokenit = _tokens.begin();
+
+   if (Exp())
+   {
+      _success = true;
+   }
+
+   return tree;
+}
+
+bool LogisParser::NextToken()
+{
+   if (_tokenit != _tokens.end()-1)
+   {
+      _tokenit++;
+      return true;
+   }
+   
+   return false;
+}
+
+//Exp -> keyword (Exp) Exp'
+//     | Text Exp'
+bool LogisParser::Exp()
+{
+   shared_ptr<LogisTreeNodeExp> node(new LogisTreeNodeExp);
+   _currentnode->AddChild(node);
+   _currentnode = node.get();
+
+
+   return false;
+}
+
+//Exp' -> + Exp''
+//      | null
+bool LogisParser::Exp1()
+{
+   shared_ptr<LogisTreeNodeExp1> node(new LogisTreeNodeExp1);
+   _currentnode->AddChild(node);
+   _currentnode = node.get();
+
+   return false;
+}
+
+//Exp'' -> keyword (Exp)
+//       | Text
+bool LogisParser::Exp2()
+{
+   shared_ptr<LogisTreeNodeExp2> node(new LogisTreeNodeExp2);
+   _currentnode->AddChild(node);
+   _currentnode = node.get();
+
+   return false;
+}
+
+//Text -> Header Factor
+//      | Factor
+bool LogisParser::Text()
+{
+   shared_ptr<LogisTreeNodeText> node(new LogisTreeNodeText);
+   _currentnode->AddChild(node);
+   _currentnode = node.get();
+
+   return false;
+}
+
+//Factor -> ( sn Tail )
+//        | sn
+bool LogisParser::Factor()
+{
+   shared_ptr<LogisTreeNodeFactor> node(new LogisTreeNodeFactor);
+   _currentnode->AddChild(node);
+   _currentnode = node.get();
+
+   return false;
+}
+
+//Tail -> + sn Tail
+//      | null
+bool LogisParser::Tail()
+{
+   shared_ptr<LogisTreeNodeTail> node(new LogisTreeNodeTail);
+   _currentnode->AddChild(node);
+   _currentnode = node.get();
+
+   return false;
+}
---
+++ Parser/LogisParser.h
@@ -0,0 +1,58 @@
+﻿//########################################################
+//##                                                    ##
+//##        Author:    xfu                              ##
+//##        E-Mail:    tech@zhongwenshu.de              ##
+//##                                                    ##
+//########################################################
+
+//物流表达式
+//%铁路(中通6666 + 圆通YT(8888 + 5641)) + %空运(JD0001-1-1)
+
+#pragma once
+
+#include <string>
+#include <vector>
+#include <memory>
+
+
+namespace XFU
+{
+   class LogisTreeNode;
+
+   class LogisParser
+   {
+   public:
+      LogisParser(const std::vector<std::wstring>& tokens)
+         :_tokens(tokens),
+         _tokenit(),
+         _success(false),
+         _currentnode(nullptr)
+      {}
+
+      ~LogisParser()
+      {}
+
+      std::shared_ptr<LogisTreeNode> Run();
+
+      bool IsSuccess() const
+      {
+         return _success;
+      }
+
+   private:
+      bool Exp();
+      bool Exp1();
+      bool Exp2();
+      bool Text();
+      bool Factor();
+      bool Tail();
+
+      bool NextToken();
+
+   private:
+      std::vector<std::wstring> _tokens;
+      std::vector<std::wstring>::const_iterator _tokenit;
+      bool _success;
+      LogisTreeNode* _currentnode;
+   };
+}
