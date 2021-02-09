a72ddf0e987202b9e3bb1aa8a1125d3cc40106dd
xfu <xfu@Dortmund.de>
2021-02-09 16:16:50 +0100

    search algorithm

---
+++ Algorithm/search.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include <limits.h>
+#include <vector>
+#include <cmath>
+
+
+
+
+int value_of_seq(const std::vector<int>& seq,int i)
+{
+   if (i <= seq.size() - 1)
+      return seq[i];
+   else
+      return INT_MAX;
+}
+
+//线性搜索，O(n)
+//如果有x，返回数列中x的位置，否则为-1
+int search(const std::vector<int>& seq,int start,int end,int x)
+{
+   if (value_of_seq(seq,start) == x)
+      return start;
+   else if (start == end)
+      return -1;
+   else
+      return search(seq,start+1,end,x);
+}
+
+//二分搜索，O(logn)
+//如果有x，返回数列中x的位置，否则为-1
+int binary_search(const std::vector<int>& seq,int i,int j,int x)
+{
+   int m = (int)floor((i + j) / 2);
+   if (value_of_seq(seq,m) == x)
+      return m;
+   else if (x<value_of_seq(seq,m) && i<m)
+      return binary_search(seq,i,m-1,x);
+   else if (x>value_of_seq(seq,m) && j>m)
+      return binary_search(seq,m+1,j,x);
+   else
+      return -1;
+}
--- footballTest/AlgorithmTest.cpp
+++ footballTest/AlgorithmTest.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "AlgorithmTest.h"
 #include "..\Algorithm\factorial.h"
+#include "..\Algorithm\search.h"
 
 using namespace std;
 using namespace XFU;
@@ -16,4 +17,17 @@ void AlgoTest::test()
    CPPUNIT_ASSERT(XFU::Factorial<5>::value==XFU::fac(5));
    CPPUNIT_ASSERT(XFU::Factorial<4>::value==XFU::factorial(4));
    CPPUNIT_ASSERT(XFU::fac(3)==XFU::factorial(3));
+
+
+   cout << "\r\ntest search...\r\n" << endl;
+
+   static const std::vector<int> lucas = {1,3,4,7,11,18,29,47,76,123};
+   CPPUNIT_ASSERT(search(lucas,0,int(lucas.size())-1,1) == 0);
+   CPPUNIT_ASSERT(binary_search(lucas,0,int(lucas.size())-1,1) == 0);
+   CPPUNIT_ASSERT(search(lucas,0,int(lucas.size())-1,29) == 6);
+   CPPUNIT_ASSERT(binary_search(lucas,0,int(lucas.size())-1,29) == 6);
+   CPPUNIT_ASSERT(search(lucas,0,int(lucas.size())-1,123) == 9);
+   CPPUNIT_ASSERT(binary_search(lucas,0,int(lucas.size())-1,123) == 9);
+   CPPUNIT_ASSERT(search(lucas,0,int(lucas.size())-1,2) == -1);
+   CPPUNIT_ASSERT(binary_search(lucas,0,int(lucas.size())-1,2) == -1);
 }
