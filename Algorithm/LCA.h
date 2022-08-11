e0f34661bb503f298f2c94ace5d98f2919cdc7b1
xfu <xfu@Dortmund.de>
2022-08-11 15:30:38 +0200

    comment

--- Algorithm/LCA.h
+++ Algorithm/LCA.h
@@ -12,4 +12,7 @@ struct TreeNode;
 class DisjointSetEx;
 
 //Tarjan’s off-line lowest common ancestors algorithm
+//O(N+Q)
+//N: number of tree nodes
+//Q: number of queries
 void __declspec(dllexport) LCA(const TreeNode* node,DisjointSetEx& ds,const std::vector<Query>& qs);
