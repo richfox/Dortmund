bf0796f55e3b5532e7ec325d3842a6f3ae36faeb
xfu <xfu@Dortmund.de>
2024-06-26 17:21:48 +0200

    test boost property_tree

--- XML/XML.cpp
+++ XML/XML.cpp
@@ -39,6 +39,19 @@ struct FetchConfig
 		{
 			_debugFiles.push_back(v.second.data());
 		}
+
+		pt::wptree ctree = tree.get_child(L"config.image.fetchRelease");
+		std::wostringstream out;
+		pt::write_xml(out, ctree);
+		auto xml = out.str();
+
+		auto opt = tree.get_child_optional(L"config.image.fetchRelease");
+		if (opt.get_ptr())
+		{
+		   std::wostringstream out;
+		   pt::write_xml(out, opt.get());
+		   auto xml2 = out.str();
+		}
 	}
 
 	void Save(const std::wstring& filename)
@@ -54,6 +67,12 @@ struct FetchConfig
 		{
 			tree.add(L"config.image.fetchDebug.fetchDebug",file);
 		}
+
+		std::wstring subxmlstr = L"<test>1</test>";
+		pt::wptree subtree;
+	    std::wistringstream iws(subxmlstr);
+	    pt::read_xml(iws, subtree);
+		tree.add_child(L"config.image.fetchRelease", subtree);
 
 		std::wofstream ofs(filename);
 		const auto settings = pt::xml_writer_make_settings<std::wstring>(L'&',3);
